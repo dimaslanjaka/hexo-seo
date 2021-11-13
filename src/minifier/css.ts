5a569f7c315031feacb4904544d4366bafea1d21
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-13 12:25:06 +0700

    +cache strategy

--- src/minifier/css.ts
+++ src/minifier/css.ts
@@ -6,34 +6,46 @@ import Hexo from "hexo";
 import { isIgnore } from "../utils";
 import log from "../log";
 import pkg from "../../package.json";
+import Cache from "../cache";
+
+const cache = new Cache();
 
 export default async function (this: Hexo, str: string, data: Hexo.View) {
-  const hexo: Hexo = this;
-  let options: defaultSeoOptions["css"] = getConfig(hexo).css;
   const path0 = data.path;
-  const exclude = typeof options == "object" ? options.exclude : [];
+  const isChanged = await cache.isFileChanged(path0);
 
-  if (path0 && exclude && exclude.length > 0) {
-    log.debug("[exclude]", isIgnore(path0, exclude), path0, exclude);
-    if (isIgnore(path0, exclude)) return str;
-  }
+  if (isChanged) {
+    // if original file is changed, re-minify js
+    const hexo: Hexo = this;
+    let options: defaultSeoOptions["css"] = getConfig(hexo).css;
+    const exclude = typeof options == "object" ? options.exclude : [];
 
-  if (typeof options === "boolean") {
-    options = {};
-  }
-
-  if (typeof options == "object") {
-    try {
-      const { styles } = await new CleanCSS(<any>options).minify(str);
-      const saved = (((str.length - styles.length) / str.length) * 100).toFixed(
-        2
-      );
-      log.log("%s(CSS): %s [%s saved]", pkg.name, path0, saved + "%");
-      str = styles;
-    } catch (err) {
-      log.log("%d(CSS) %s failed", pkg.name, path0);
-      log.error(err);
+    if (path0 && exclude && exclude.length > 0) {
+      log.debug("[exclude]", isIgnore(path0, exclude), path0, exclude);
+      if (isIgnore(path0, exclude)) return str;
     }
+
+    if (typeof options === "boolean") {
+      options = {};
+    }
+
+    if (typeof options == "object") {
+      try {
+        const { styles } = await new CleanCSS(<any>options).minify(str);
+        const saved = (
+          ((str.length - styles.length) / str.length) *
+          100
+        ).toFixed(2);
+        log.log("%s(CSS): %s [%s saved]", pkg.name, path0, saved + "%");
+        str = styles;
+        cache.set(path0, str);
+      } catch (err) {
+        log.log("%d(CSS) %s failed", pkg.name, path0);
+        log.error(err);
+      }
+    }
+  } else {
+    str = cache.get(path0);
   }
 
   return str;
