e6c68edbf0125bc00211d25641a866dae5850e55
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-14 15:12:28 +0700

    fix hexo-seo

--- src/hexo/hexo-is/index.ts
+++ src/hexo/hexo-is/index.ts
@@ -10,24 +10,17 @@ const log = hexoLog({
   silent: false
 });
 
+/**
+ * @example
+ * // run inside plugin or theme event
+ * import hexoIs from 'hexo-is';
+ * const hexo = this;
+ * console.log(hexoIs(hexo));
+ * @param hexo
+ * @returns
+ */
 const hexoIs = function (hexo: Hexo | Hexo.View) {
-  if (typeof hexo["path"] != "undefined") {
-    //console.log("using property path");
-    //hexoIsDump(hexo);
-    return validate(hexo);
-  }
-  if (typeof hexo["page"] != "undefined") {
-    //console.log("using property page");
-    return validate(hexo["page"]);
-  }
-  if (typeof hexo["url"] != "undefined") {
-    //console.log("using property url");
-    return validate(hexo["url"]);
-  }
-  //console.log(hexo["view"]);
-  hexoIsDump(hexo);
-  log.error("%s: hexo instance not found", pkg.name);
-  return null;
+  return is(hexo);
 };
 
 /**
@@ -39,7 +32,7 @@ let dumped = false;
  * Dump variable to file
  * @param toDump
  */
-function hexoIsDump(toDump: any) {
+export function hexoIsDump(toDump: any) {
   if (dumped) return;
   dumped = true;
   const dump = util.inspect(toDump);
@@ -49,35 +42,6 @@ function hexoIsDump(toDump: any) {
   }
   fs.writeFileSync(loc, dump);
   log.log(`${pkg.name}: dump saved to: ${path.resolve(loc)}`);
-}
-
-function validate(pathx: any) {
-  const indexTest = {
-    archive: Boolean(pathx.archive),
-    tag: Boolean(pathx.tag),
-    category: Boolean(pathx.category),
-    post: Boolean(pathx.__post),
-    page: Boolean(pathx.__page),
-    home: Boolean(pathx.__index)
-  };
-  //console.log(indexTest);
-  if (typeof pathx.path === "string") {
-    //console.log(pathx);
-    pathx = pathx.path;
-  }
-
-  if (typeof pathx == "string") {
-    pathx = pathx.replace(/^\//, "");
-    //console.log(pathx);
-    return {
-      archive: /archive?s\//gs.test(pathx),
-      tag: /tag?s\//gs.test(pathx),
-      category: /category|categories\//gs.test(pathx)
-    };
-  }
-
-  // https://github.com/hexojs/hexo/blob/master/lib/plugins/helper/is.js
-  return indexTest;
 }
 
 export default hexoIs;
--- src/hexo/hexo-is/readme.md
+++ src/hexo/hexo-is/readme.md
@@ -1,3 +1,15 @@
 # Determine hexo instance
 
 > Where is you now ? page ? archive ? whatever
+
+# Usage
+
+```js
+const is = require('hexo-is');
+function where(){
+  console.log(is(this));
+}
+hexo.extend.filter.register("after_render:js", where);
+hexo.extend.filter.register("after_render:css", where);
+hexo.extend.filter.register("after_render:html", where);
+```
