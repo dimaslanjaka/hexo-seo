1c07eb390ca02509a463b00230f5918e3c2357a3
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-17 11:15:11 +0700

    strip image broker caused heap memory

--- dist/src/img/fixAttributes.js
+++ dist/src/img/fixAttributes.js
@@ -1,38 +1,88 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var cheerio_1 = __importDefault(require("cheerio"));
+//import Promise from "bluebird";
 var cache_1 = __importDefault(require("../cache"));
 var cache = new cache_1.default();
 function default_1(content, data) {
-    var path0 = data.path;
-    return cache.isFileChanged(path0).then(function (isChanged) {
-        console.log("changed", isChanged, path0);
-        if (isChanged) {
-            var $_1 = cheerio_1.default.load(content);
-            //const config = getConfig(this).img;
-            var title_1 = data.title;
-            $_1("img").each(function (i, el) {
-                var img = $_1(el);
-                var img_alt = img.attr("alt");
-                var img_title = img.attr("title");
-                var img_itemprop = img.attr("itemprop");
-                if (!img_alt || img_alt.trim().length === 0) {
-                    img.attr("alt", title_1);
-                }
-                if (!img_title || img_title.trim().length === 0) {
-                    img.attr("title", title_1);
-                }
-                if (!img_itemprop || img_itemprop.trim().length === 0) {
-                    img.attr("itemprop", "image");
-                }
-            });
-            cache.set(path0, $_1.html());
-            return $_1.html();
-        }
-        return cache.get(path0);
+    return __awaiter(this, void 0, void 0, function () {
+        var path0, isChanged, config, $_1, title_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    path0 = data.path;
+                    return [4 /*yield*/, cache.isFileChanged(path0)];
+                case 1:
+                    isChanged = _a.sent();
+                    config = this.config;
+                    console.log("changed", isChanged, path0);
+                    if (isChanged) {
+                        $_1 = cheerio_1.default.load(content);
+                        title_1 = data.title || config.title;
+                        $_1("img").each(function (i, el) {
+                            var img = $_1(el);
+                            var img_alt = img.attr("alt");
+                            var img_title = img.attr("title");
+                            var img_itemprop = img.attr("itemprop");
+                            //const img_src = img.attr("src");
+                            if (!img_alt || img_alt.trim().length === 0) {
+                                img.attr("alt", title_1);
+                            }
+                            if (!img_title || img_title.trim().length === 0) {
+                                img.attr("title", title_1);
+                            }
+                            if (!img_itemprop || img_itemprop.trim().length === 0) {
+                                img.attr("itemprop", "image");
+                            }
+                        });
+                        cache.set(path0, $_1.html());
+                        return [2 /*return*/, $_1.html()];
+                    }
+                    else {
+                        return [2 /*return*/, cache.get(path0)];
+                    }
+                    return [2 /*return*/];
+            }
+        });
     });
 }
 exports.default = default_1;
--- src/img/fixAttributes.ts
+++ src/img/fixAttributes.ts
@@ -4,42 +4,44 @@ import cheerio, { Cheerio, Element } from "cheerio";
 import Hexo from "hexo";
 import getConfig from "../config";
 import checkUrl from "../curl/check";
-import Promise from "bluebird";
+//import Promise from "bluebird";
 import InMemoryCache from "../cache";
 
 const cache = new InMemoryCache();
 
-export default function (
+export default async function (
   this: Hexo,
   content: string,
   data: HexoSeo
 ): globalThis.Promise<string> {
   const path0 = data.path;
-  return cache.isFileChanged(path0).then((isChanged) => {
-    console.log("changed", isChanged, path0);
-    if (isChanged) {
-      const $ = cheerio.load(content);
-      //const config = getConfig(this).img;
-      const title = data.title;
-      $("img").each((i, el) => {
-        const img = $(el);
-        const img_alt = img.attr("alt");
-        const img_title = img.attr("title");
-        const img_itemprop = img.attr("itemprop");
-        if (!img_alt || img_alt.trim().length === 0) {
-          img.attr("alt", title);
-        }
-        if (!img_title || img_title.trim().length === 0) {
-          img.attr("title", title);
-        }
-        if (!img_itemprop || img_itemprop.trim().length === 0) {
-          img.attr("itemprop", "image");
-        }
-      });
+  const isChanged = await cache.isFileChanged(path0);
+  const config = this.config;
+  console.log("changed", isChanged, path0);
+  if (isChanged) {
+    const $ = cheerio.load(content);
+    //const config = getConfig(this).img;
+    const title = data.title || config.title;
+    $("img").each((i, el) => {
+      const img = $(el);
+      const img_alt = img.attr("alt");
+      const img_title = img.attr("title");
+      const img_itemprop = img.attr("itemprop");
+      //const img_src = img.attr("src");
+      if (!img_alt || img_alt.trim().length === 0) {
+        img.attr("alt", title);
+      }
+      if (!img_title || img_title.trim().length === 0) {
+        img.attr("title", title);
+      }
+      if (!img_itemprop || img_itemprop.trim().length === 0) {
+        img.attr("itemprop", "image");
+      }
+    });
 
-      cache.set(path0, $.html());
-      return $.html();
-    }
+    cache.set(path0, $.html());
+    return $.html();
+  } else {
     return cache.get(path0);
-  });
+  }
 }
