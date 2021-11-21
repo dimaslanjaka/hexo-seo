272e97ec24de8238a3472357e7940a6434d80a1f
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-21 14:32:14 +0700

    fix invalid html

--- dist/src/html/fixInvalid.js
+++ dist/src/html/fixInvalid.js
@@ -4,10 +4,11 @@ var jsdom_1 = require("jsdom");
 function default_1(content) {
     var dom = new jsdom_1.JSDOM(content);
     var document = dom.window.document;
-    var cssx = document.querySelectorAll('link[rel="stylesheet"][href="/.css"]');
+    var cssx = document.querySelectorAll('*[href="/.css"],*[src="/.js"]');
     cssx.forEach(function (i) {
         i.outerHTML = "<!-- invalid " + i.outerHTML + " -->";
     });
+    content = document.documentElement.outerHTML;
     return content;
 }
 exports.default = default_1;
--- src/html/fixInvalid.ts
+++ src/html/fixInvalid.ts
@@ -4,11 +4,10 @@ import { JSDOM } from "jsdom";
 export default function (this: Hexo, content: string) {
   const dom = new JSDOM(content);
   const document: Document = dom.window.document;
-  const cssx = document.querySelectorAll(
-    'link[rel="stylesheet"][href="/.css"]'
-  );
+  const cssx = document.querySelectorAll('*[href="/.css"],*[src="/.js"]');
   cssx.forEach((i) => {
     i.outerHTML = `<!-- invalid ${i.outerHTML} -->`;
   });
+  content = document.documentElement.outerHTML;
   return content;
 }
