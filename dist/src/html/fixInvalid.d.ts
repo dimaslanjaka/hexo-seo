9a32119e563dbb8d2f672173ec836a9f3fd4f51d
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-22 20:36:41 +0700

    Fix cache

--- dist/src/html/fixInvalid.d.ts
+++
@@ -1,4 +0,0 @@
-import Hexo from "hexo";
-import { HexoSeo } from "./schema/article";
-declare function fixInvalid(this: Hexo, content: string, data: HexoSeo): string;
-export default fixInvalid;
--- dist/src/html/fixSchema.d.ts
+++
@@ -1,6 +0,0 @@
-import Hexo from "hexo";
-import { HexoSeo } from "./schema/article";
-import "../../packages/js-prototypes/src/String";
-import "../../packages/js-prototypes/src/Array";
-declare function fixSchema(this: Hexo, content: string, data: HexoSeo): string;
-export default fixSchema;
