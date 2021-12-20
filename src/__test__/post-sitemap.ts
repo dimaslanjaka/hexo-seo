0598ad32125ecd2a2eb1e6ea65410837e3661963
dimaslanjaka <dimaslanjaka@gmail.com>
2021-12-21 05:51:23 +0700

    update definition hexo

--- src/__test__/post-sitemap.ts
+++ src/__test__/post-sitemap.ts
@@ -1,10 +1,11 @@
 import Hexo from "hexo";
 import moment from "moment";
 import { MutableRequired, ReadonlyPartial } from "../utils/types";
-import Promise from "bluebird";
 import { join } from "path";
 import yaml from "yaml";
 import { readFileSync } from "fs";
+import HexoConfig from "hexo/HexoConfig";
+import Bluebird from "bluebird";
 const yml = yaml.parse(readFileSync(join(__dirname, "/../../_config.yml")).toString());
 
 /**
@@ -14,13 +15,13 @@ const yml = yaml.parse(readFileSync(join(__dirname, "/../../_config.yml")).toStr
 function instanciateHexo() {
   const hexo: MutableRequired<Hexo> = new Hexo(__dirname, { silent: true });
   // setup config
-  const config = <MutableRequired<typeof hexo.config>>hexo.config;
+  const config: MutableRequired<typeof hexo.config> = Object.assign(hexo.config, yml);
   config.permalink = ":title";
   config.url = "http://webmanajemen.com";
   // re-append
-  hexo.config = Object.assign(hexo.config, yml);
+  hexo.config = config;
   hexo.init();
-  return Promise.resolve(<Hexo>hexo);
+  return Bluebird.resolve(<Hexo>hexo);
 }
 
 /**
@@ -44,10 +45,13 @@ const setPostTag = function (hexo: Hexo, posts) {
 };
 
 const getHexoLocalsAndConfig = function (hexo: Hexo) {
-  return Promise.resolve([hexo.locals.toObject(), hexo.config]);
+  return Bluebird.resolve([hexo.locals.toObject(), hexo.config]) as Bluebird<
+    [ReturnType<typeof hexo.locals.toObject>, HexoConfig]
+  >;
 };
 
 const init = instanciateHexo().then(insertPosts).spread(setPostTag).spread(getHexoLocalsAndConfig);
 init.then((result) => {
-  console.log(result);
+  const config = result[1];
+  const hexo = result[0];
 });
