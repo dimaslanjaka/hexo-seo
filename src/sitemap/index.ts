d775d26a2324c028cf73b38318201b024d0a7fcb
dimaslanjaka <dimaslanjaka@gmail.com>
2021-12-22 00:09:40 +0700

    improve sitemap

--- src/sitemap/index.ts
+++ src/sitemap/index.ts
@@ -2,7 +2,7 @@ import Hexo, { TemplateLocals } from "hexo";
 import moment from "moment";
 import { getCacheFolder } from "../utils";
 import { create as createXML } from "xmlbuilder2";
-import { readFileSync, statSync } from "fs";
+import { copyFile, copyFileSync, existsSync, readFileSync, statSync } from "fs";
 import { join } from "path";
 import hexoIs from "../hexo/hexo-is";
 import { HexoIs } from "../hexo/hexo-is/is";
@@ -22,78 +22,92 @@ interface sitemapObj {
     url: sitemapItem[];
   };
 }
-const doc = createXML(readFileSync(join(__dirname, "views/post-sitemap.xml")).toString());
-const obj = <sitemapObj>new Object(doc.end({ format: "object" }));
-obj.urlset.url = [];
+interface sitemapGroup {
+  post: sitemapObj;
+  page: sitemapObj;
+}
+const sitemapGroup: sitemapGroup = {
+  post: undefined,
+  page: undefined
+};
 
-interface returnPage extends Hexo.PageData {
+function initSitemap(type: string | "post" | "page") {
+  if (!sitemapGroup[type]) {
+    const sourceXML = join(__dirname, "views/" + type + "-sitemap.xml");
+    if (!existsSync(sourceXML)) throw "Source " + sourceXML + " Not Found";
+    const doc = createXML(readFileSync(sourceXML).toString());
+    sitemapGroup[type] = <sitemapObj>new Object(doc.end({ format: "object" }));
+    sitemapGroup[type].urlset.url = [];
+  }
+}
+
+export interface returnPageData extends Hexo.PageData {
   [key: string]: any;
   is: HexoIs;
 }
+/**
+ * Extract Page Data
+ * @param data
+ * @returns
+ */
 export function getPageData(data: TemplateLocals) {
   const is = hexoIs(data);
   if (data["page"]) {
-    const page = <returnPage>data["page"];
+    const page = <returnPageData>data["page"];
     page.is = is;
     return page;
   }
 }
 
-export function sitemap_post(this: Hexo, content: string, data: TemplateLocals) {
+export function sitemap(this: Hexo, _content: string, data: TemplateLocals) {
   const hexo = this;
   const locals = hexo.locals;
   if (locals.get("posts").length === 0) {
     return;
   }
-  if (obj.urlset.url.length === 0)
-    obj.urlset.url.push({
-      loc: hexo.config.url,
-      lastmod: moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
-      priority: "1",
-      changefreq: "daily"
-    });
+  const groups = ["post", "page"];
+  groups.forEach((group) => {
+    if (!sitemapGroup[group]) initSitemap(group);
+    if (sitemapGroup[group].urlset.url.length === 0) {
+      sitemapGroup[group].urlset.url.push({
+        loc: hexo.config.url,
+        lastmod: moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
+        priority: "1",
+        changefreq: "daily"
+      });
+    }
+  });
   const post = getPageData(data);
   if (post) {
-    if (post.is.post) {
+    scheduler.add("copySitemapXSL", () => {
+      // copy xsl
+      const destXSL = join(hexo.public_dir, "sitemap.xsl");
+      const sourceXSL = join(__dirname, "views/sitemap.xsl");
+      copyFile(sourceXSL, destXSL, (err) => {
+        if (!err) log.log("XSL sitemap copied to " + destXSL);
+      });
+    });
+
+    if (post.is.post || post.is.page) {
       if (!post.updated) {
         const stats = statSync(post.full_source);
         post.updated = moment(stats.mtime);
       }
-      obj.urlset.url.push({
+    }
+    if (post.is.post) {
+      sitemapGroup["post"].urlset.url.push({
         loc: post.permalink,
         lastmod: post.updated.format("YYYY-MM-DDTHH:mm:ssZ"),
         changefreq: "weekly",
         priority: "0.6"
       });
+
+      scheduler.add("writeSitemapPost", () => {
+        const destPostSitemap = join(hexo.public_dir, "post-sitemap.xml");
+        log.log("post sitemap saved", destPostSitemap);
+        writeFile(destPostSitemap, createXML(sitemapGroup["post"]).end({ prettyPrint: true }));
+      });
     }
-
-    const temp = join(hexo.public_dir, "post-sitemap.xml");
-
-    scheduler.add("writeCacheFile", () => {
-      log.log("sitemap saved", temp);
-      writeFile(temp, createXML(obj).end({ prettyPrint: true }));
-    });
   }
-
-  /*
-  const posts = _(locals.get("posts").toArray())
-    .filter((post) => {
-      if ([post.sitemap, post.indexing].some((b) => b === false)) return false;
-      return post.published;
-    })
-    .orderBy("updated", "desc")
-    .value();
-  const post = posts.find((post) =>
-    post.title && page.title ? post.title.toLowerCase() === page.title.toLowerCase() : false
-  );
-  if (post) {
-    const build = {
-      url: {
-        loc: post.permalink,
-        lastmod: post.updated
-      }
-    };
-    console.log(build);
-  }*/
 }
-export default sitemap_post;
+export default sitemap;
