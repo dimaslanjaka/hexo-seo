e45d204a16873b5f6313899b26050bc7f06241d2
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-11 16:39:22 +0700

    Update packer.js

--- packer.js
+++ packer.js
@@ -1,30 +1,32 @@
 /* eslint-disable no-useless-escape */
-const { spawn } = require("cross-spawn");
-const { readdirSync, createReadStream } = require("fs");
-const { existsSync, renameSync, rmSync, mkdirpSync, writeFileSync } = require("fs-extra");
-const GulpClient = require("gulp");
-const { join, dirname } = require("upath");
-const packagejson = require("./package.json");
-const crypto = require("crypto");
+const { spawn } = require('cross-spawn');
+const { readdirSync, createReadStream, readFileSync } = require('fs');
+const { existsSync, renameSync, rmSync, mkdirpSync, writeFileSync } = require('fs-extra');
+const GulpClient = require('gulp');
+const { join, dirname, toUnix } = require('upath');
+const packagejson = require('./package.json');
+const crypto = require('crypto');
+// const os = require('os');
 
 // auto create tarball (tgz) on release folder
-// raw: https://raw.githubusercontent.com/dimaslanjaka/static-blog-generator-hexo/master/packages/gulp-sbg/packer.js
-// github: https://github.com/dimaslanjaka/static-blog-generator-hexo/blob/master/packages/gulp-sbg/packer.js
-// update: curl https://raw.githubusercontent.com/dimaslanjaka/static-blog-generator-hexo/master/packages/gulp-sbg/packer.js > packer.js
-// usage: node packer.js
+// raw            : https://github.com/dimaslanjaka/nodejs-package-types/raw/main/packer.js
+// github         : https://github.com/dimaslanjaka/nodejs-package-types/blob/main/packer.js
+// update         : curl -L https://github.com/dimaslanjaka/nodejs-package-types/raw/main/packer.js > packer.js
+// usage          : node packer.js
+// github actions : https://github.com/dimaslanjaka/nodejs-package-types/blob/main/.github/workflows/build-release.yml
 
-console.log("=".repeat(19));
-console.log("= packing started =");
-console.log("=".repeat(19));
+console.log('='.repeat(19));
+console.log('= packing started =');
+console.log('='.repeat(19));
 
-const releaseDir = join(__dirname, "release");
-const child = spawn("npm", ["pack"], { cwd: __dirname, stdio: "ignore" });
+const releaseDir = join(__dirname, 'release');
+const child = spawn('npm', ['pack'], { cwd: __dirname, stdio: 'ignore' });
 let version = (function () {
   const v = parseVersion(packagejson.version);
   return `${v.major}.${v.minor}.${v.patch}`;
 })();
 
-child.on("exit", function () {
+child.on('exit', function () {
   const filename = slugifyPkgName(`${packagejson.name}-${version}.tgz`);
   const tgz = join(__dirname, filename);
 
@@ -37,31 +39,27 @@ child.on("exit", function () {
 
   const getPackageHashes = function () {
     return new Promise(function (resolve) {
-      let data = {};
-      let pkglock = [join(__dirname, "package-lock.json"), join(__dirname, "yarn.lock")].filter((str) =>
+      let hashes = {};
+      const metafile = join(releaseDir, 'metadata.json');
+      // read old meta
+      if (existsSync(metafile)) {
+        hashes = Object.assign(hashes, JSON.parse(readFileSync(metafile, 'utf-8')));
+      }
+      let pkglock = [join(__dirname, 'package-lock.json'), join(__dirname, 'yarn.lock')].filter((str) =>
         existsSync(str)
       )[0];
       const readDir = readdirSync(releaseDir)
-        .filter((path) => path.endsWith("tgz"))
+        .filter((path) => path.endsWith('tgz'))
         .map((path) => join(releaseDir, path));
 
-      if (typeof pkglock === "string" && existsSync(pkglock)) {
+      if (typeof pkglock === 'string' && existsSync(pkglock)) {
         readDir.push(pkglock);
       }
       readDir.forEach((file, index, all) => {
-        const sha1 = (path) =>
-          new Promise((resolve, reject) => {
-            const hash = crypto.createHash("sha1");
-            const rs = createReadStream(path);
-            rs.on("error", reject);
-            rs.on("data", (chunk) => hash.update(chunk));
-            rs.on("end", () => resolve(hash.digest("hex")));
-          });
-
         sha1(file)
           .then((hash) => {
-            data = Object.assign({}, data, {
-              [file]: {
+            hashes = Object.assign({}, hashes, {
+              [toUnix(file).replace(toUnix(__dirname), '')]: {
                 hash
               }
             });
@@ -72,7 +70,12 @@ child.on("exit", function () {
           .finally(() => {
             if (index === all.length - 1) {
               //console.log("Last callback call at index " + index + " with value " + file);
-              resolve(data);
+
+              //hashes = { [os.type()]: { [os.arch()]: hashes } };
+              writeFileSync(metafile, JSON.stringify(hashes, null, 2));
+              console.log(hashes);
+
+              resolve(hashes);
             }
           });
       });
@@ -86,7 +89,7 @@ child.on("exit", function () {
   if (existsSync(tgz)) {
     GulpClient.src(tgz)
       .pipe(GulpClient.dest(releaseDir))
-      .once("end", function () {
+      .once('end', function () {
         if (existsSync(tgzlatest)) {
           rmSync(tgzlatest);
         }
@@ -95,20 +98,17 @@ child.on("exit", function () {
         if (existsSync(tgz)) rmSync(tgz);
 
         // write hashes info
-        getPackageHashes().then((hashes) => {
-          writeFileSync(join(releaseDir, "metadata.json"), JSON.stringify(hashes, null, 2));
-          console.log(hashes);
+        getPackageHashes().then(function () {
+          console.log('='.repeat(20));
+          console.log('= packing finished =');
+          console.log('='.repeat(20));
         });
-
-        console.log("=".repeat(20));
-        console.log("= packing finished =");
-        console.log("=".repeat(20));
       });
   }
 });
 
 function slugifyPkgName(str) {
-  return str.replace(/\//g, "-").replace(/@/g, "");
+  return str.replace(/\//g, '-').replace(/@/g, '');
 }
 
 /**
@@ -117,14 +117,14 @@ function slugifyPkgName(str) {
  * @returns
  */
 function parseVersion(versionString) {
-  var vparts = versionString.split(".");
+  var vparts = versionString.split('.');
   const version = {
     major: parseInt(vparts[0]),
     minor: parseInt(vparts[1]),
-    patch: parseInt(vparts[2].split("-")[0]),
+    patch: parseInt(vparts[2].split('-')[0]),
     build: parseInt(vparts[3] || null),
-    range: parseInt(vparts[2].split("-")[1]),
-    commit: vparts[2].split("-")[2]
+    range: parseInt(vparts[2].split('-')[1]),
+    commit: vparts[2].split('-')[2]
   };
 
   return version;
@@ -132,7 +132,7 @@ function parseVersion(versionString) {
 
 function addReadMe() {
   writeFileSync(
-    join(releaseDir, "readme.md"),
+    join(releaseDir, 'readme.md'),
     `
 # Release \`${packagejson.name}\` Tarball
 
@@ -149,11 +149,21 @@ npm i https://....url-tgz
 \`\`\`
 for example
 \`\`\`bash
-npm i https://github.com/dimaslanjaka/static-blog-generator-hexo/raw/master/packages/gulp-sbg/release/static-blog-generator.tgz
+npm i https://github.com/dimaslanjaka/nodejs-package-types/raw/main/release/nodejs-package-types.tgz
 \`\`\`
 
 ## URL Parts Explanations
 > https://github.com/github-username/github-repo-name/raw/github-branch-name/path-to-file-with-extension
   `.trim()
   );
+}
+
+function sha1(path) {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash('sha1');
+    const rs = createReadStream(path);
+    rs.on('error', reject);
+    rs.on('data', (chunk) => hash.update(chunk));
+    rs.on('end', () => resolve(hash.digest('hex')));
+  });
 }
