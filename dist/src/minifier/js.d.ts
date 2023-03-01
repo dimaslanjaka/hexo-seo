625be50c5c8691e8aeed5edc5611af7b6da290fe
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-01 12:45:41 +0700

    hotfix merge conflicts

--- dist/src/minifier/js.d.ts
+++ dist/src/minifier/js.d.ts
@@ -7,4 +7,20 @@ export interface jsMinifyOptions {
     exclude?: string[];
     options?: MinifyOptions;
 }
-export default function HexoSeoJs(this: Hexo, str: string, data: Hexo.View): Promise<string>;
+/**
+ * minify js
+ * @param this
+ * @param str
+ * @param data
+ * @returns
+ */
+export default function HexoSeoJs(this: Hexo, str: string, data: Hexo.View | {
+    path: string;
+}): Promise<string>;
+/**
+ * minify js
+ * @param str
+ * @param options
+ * @returns
+ */
+export declare function minifyJS(str: string, options: MinifyOptions): Promise<string>;
