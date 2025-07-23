bccb7412d14e38fe3240328db89cc0cca600f943
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-23 23:19:40 +0700

    fix(env): update dotenv configuration to use dynamic envPath based on hexo context

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
+import path from 'path';
 import HexoSeo from './hexo-seo';
 
-dotenv.config({ override: true });
+let envPath = path.join(process.cwd(), '.env');
+if (typeof hexo !== 'undefined') {
+  envPath = path.join(hexo.base_dir, '.env');
+}
+dotenv.config({ override: true, path: envPath });
 
 if (typeof hexo !== 'undefined') {
   HexoSeo(hexo);
