7226cfe0281dae189a73ef3ec012d736b0880ccb
Dimas Lanjaka <dimaslanjaka@gmail.com>
2025-07-25 16:59:20 +0700

    feat(test): enhance getPageData tests with JestLogger and random post generation

--- test/sitemap/getPageData.test.ts
+++ test/sitemap/getPageData.test.ts
@@ -1,7 +1,12 @@
+import fs from 'fs-extra';
 import Hexo from 'hexo';
+import path from 'path';
 import { modifyHexoConfig } from '../../pretest.cjs';
 import { baseSite } from '../env.cjs';
+import JestLogger from '../jest-logger.cjs';
+import { generateRandomMarkdownPosts } from '../utils.cjs';
 
+const logger = new JestLogger('getPageData');
 let hexo: Hexo;
 let sitemapModule: typeof import('../../src/sitemap/index');
 const config = {
@@ -16,20 +21,38 @@ const config = {
 
 jest.setTimeout(120000);
 
-beforeAll(async () => {
-  hexo = new Hexo(baseSite, { silent: true });
-  modifyHexoConfig(config);
-  await hexo.init();
-  await hexo.call('clean');
-  (global as any).hexo = hexo;
-  // Dynamically import sitemapModule after global hexo is set
-  sitemapModule = await import('../../dist/src/sitemap/index.js');
-});
-
 describe('getPageData', () => {
+  const generatedMarkdownPosts = generateRandomMarkdownPosts(
+    path.join(baseSite, 'source/_posts'),
+    10,
+    ['js', 'ts'],
+    ['programming', 'web development']
+  );
+
+  beforeAll(async () => {
+    hexo = new Hexo(baseSite, { silent: true });
+    modifyHexoConfig(config);
+    await hexo.init();
+    await hexo.load();
+    await hexo.call('clean');
+    await hexo.call('generate');
+    // Set global hexo instance for the test environment
+    (global as any).hexo = hexo;
+    // Dynamically import sitemapModule after global hexo is set
+    sitemapModule = await import('../../dist/src/sitemap/index.js');
+  });
+
+  afterAll(() => {
+    generatedMarkdownPosts.forEach((filename) => {
+      const filePath = path.join(baseSite, 'source/_posts', filename);
+      fs.rmSync(filePath, { force: true, recursive: true });
+    });
+  });
+
   it('returns page data with "is" and "title" properties for a valid post', () => {
     const posts = hexo.locals.get('posts');
     const page = posts.data ? posts.data[0] : posts[0];
+    logger.log(posts);
     const data = { page };
     const result = sitemapModule.getPageData.call(hexo, data);
     expect(result).toHaveProperty('is');
